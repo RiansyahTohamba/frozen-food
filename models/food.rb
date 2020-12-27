7f3d7234a34f254550295fbe254aad75d486b72e
muhammad riansyah <mriansyah93@gmail.com>
2020-12-28 04:57:58 +0700

    add food and spec

--- models/food.rb
+++ models/food.rb
@@ -1,7 +1,19 @@
+require 'date'
 class Food
 	attr :type, :expired
+	
 	def initialize(type,expired)
 	  @type = type
 	  @expired = expired
+	end
+
+	def shelflife
+		if @type.eql? 'sayap'
+			@expired = Date.today.next_year
+		end				
+	end
+
+	def show_detail(shelflife)
+
 	end
 end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -1,9 +1,23 @@
 # file:///home/riansyah/Documents/lainnya/ebook/effective-testing-with-rspec-3_p1_0.pdf
 require_relative '../models/food'
 RSpec.describe 'An ideal Sayap ' do
-	it 'is sayap' do
-		# expect(ledger).to receive(:records)
+	it 'is adding the ppn' do
+		# STUB
+		food = double("Food")
+		allow(food).to receive(:count_price).and_return(200)
 	end
+
+	it 'is show the detail of the food' do
+		# MOCK example
+		# real object
+		food = new Food('sayap','10-10-2021')
+		# mock object
+		shelflife = double('shelflife')
+		# check if whether method get called or not 
+		expect(shelflife).to receive(:shelflife).with('10-10-2020')
+		food.show_detail(shelflife)
+	end
+
 	it 'is sayap' do
 		syp = Food.new('sayap', '10-10-2021')
 		
